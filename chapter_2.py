87545eaf52696853769f59f51f4ef7e33fe8c3c9
ziaul sarker <ziaulsarker@gmail.com>
2023-12-17 20:54:46 -0500

    chapter 2

---
+++ chapter_2.py
@@ -0,0 +1,57 @@
+# 2.2
+some_msg = "some message"
+print(some_msg)
+
+some_msg = "some message changed"
+print(some_msg)
+
+# 2.3
+me = "Ziaul Sarker"
+msg = f'yoo bithc ass {me}, whats cracking nigga'
+print(msg)
+
+# 2.4
+ziaul = "ziaul SArker"
+print(f'''
+  title -> {ziaul.title()} \n 
+  lower -> {ziaul.upper()} \n  
+  upper -> {ziaul.lower()} \n
+''')
+# 2.5, 2.6, 2.7
+mj_quote = "I have failed over and over again and that is why i succeeded"
+goat = " \n Michael Jordan   "
+print(f'{goat}: "{mj_quote}"')
+
+print(f'no strip->{goat}')
+print(f'left strip->{goat.lstrip()}')
+print(f'right strip->{goat.rstrip()}')
+print(f'strip->{goat.strip()}')
+
+print(f'goat->{goat}')
+
+
+#2.8
+
+txt = 'resume.txt'
+print(txt)
+print(txt.removesuffix('.txt'))
+
+if(txt.endswith('.txt')): 
+  print(txt[:-4])
+
+
+# 2.9
+#+-*/
+  
+print(5+8)
+print(11 - 3)
+print(4 * 2)
+print(round((16 / 2)))
+
+faveotite_num = 11
+
+print(faveotite_num)
+
+
+
+
