c9801193997335a00fe26100a1e4d85881ba2679
ziaul sarker <ziaulsarker@gmail.com>
2023-12-20 23:05:28 -0500

    chapter 5 lists complete

--- chapter_5.py
+++ chapter_5.py
@@ -70,3 +70,72 @@ arr = [1,2,3,4]
 print(1 in arr)
 
 print(10  in arr)
+
+# 5.3 
+
+aliens_color = "red"
+
+if(aliens_color == "green"): 
+  print('got 5 points')
+elif (aliens_color == "yellow"): 
+  print('got 10 points')
+elif (aliens_color == "red"): 
+    print('got 15 points')
+
+
+age = 32
+
+if (age < 2):
+  print('you a baby')
+elif (age < 4):
+  print('u a todler')
+elif (age < 13):
+  print('you a kid')
+elif (age < 20): 
+  print('you a teen')
+elif age < 65 :
+  print ('you are an adult')
+elif ( age >= 65):
+  print("u old as fuckk ")
+
+
+favorite_fruits = ['mango', 'watermelon', 'apple']
+
+other_fruits = ['blueberries', 'bananas', 'orange', 'peach', 'mango', "limes"]
+
+for fruit in other_fruits :
+  if fruit in favorite_fruits: 
+    print(f'your bitch ass realluy like {fruit}')
+  else : print(f'your bitch DONT LIKE {fruit.upper()}')
+
+  
+print("fuck fruits")
+
+
+# 5.8
+users = ["ziaul", "admin", "mike", "bill", "larry"]
+
+if not users: 
+    print('no user found')
+
+for user in users :
+  if(user.lower() == 'admin'): 
+    print(f'hello {user}')
+  else :
+    print(f'hello {user} u ugly motherfucker')
+
+
+#5.10
+active_users = users[:]
+
+new_useres = active_users[2:4] + ['ross', 'lebron']
+
+for user in new_useres :
+  if(user.lower() in active_users): 
+    print(f'sorry {user} is already in use')
+  else :
+    print(f"ohh you good {user} mother fucker")
+  
+
+
+   
