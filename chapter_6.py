89afa10893209c1808500a066bbefc168d695aaf
ziaul sarker <ziaulsarker@gmail.com>
2023-12-25 01:28:11 -0500

    finish chapter 6

--- chapter_6.py
+++ chapter_6.py
@@ -27,3 +27,85 @@ glossary = {
 
 
 print(glossary['clousures'])
+
+
+
+
+# 6.5
+
+rivers = {
+  'missippie': 'luisiana',
+  'goerge': "NY",
+  'ganji': "india",
+}
+
+
+for river, location in rivers.items() :
+  print(f'\n the {river} is in {location}')
+
+
+# 6.6
+  
+favorite_languages = {
+  "bill": 'C++',
+  "rob": 'Python',
+  "ziaul": "js"
+}
+  
+people_taken_poll = ['ziaul', 'mike', 'rob' ]
+
+for person in favorite_languages : 
+  if person not in people_taken_poll :
+    print(f'\n{person} please take out poll')
+  
+  if person in people_taken_poll :
+    print(f'\nyo bitch ass {person}, thanks for taking the pill!!')
+
+
+# 6.7
+    
+me = {
+  'first_name': 'Ziaul',
+  'last_name': 'Sarker',
+  'age': 33,
+  'city': 'Long Island'
+}
+
+amena =  {
+  'first_name': 'Amena',
+  'last_name': 'Pat',
+  'age': 21,
+  'city': 'Long Island'
+}
+
+mike =  {
+  'first_name': 'Mike',
+  'last_name': 'Rob',
+  'age': 30,
+  'city': 'Canada'
+}
+
+
+peoples = [me, amena, mike]
+
+for person in peoples : 
+  print(f'hi im {person['first_name'].upper()} here are some stuff about me ')
+  for infor in person : 
+    print(f'\n {infor} : {person[infor]} \n')
+
+
+
+niggas_fav_number = {
+  "bill": [0, 1],
+  'rob': [100, 11],
+  'mike': [5, 2],
+}
+
+for person in niggas_fav_number :
+  print(f'hi im {person}, my favorite numbers are', *niggas_fav_number[person])
+
+my_set = {'uoo', 1, 2,3 }
+
+print(my_set)
+
+
