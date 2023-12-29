9e3d92ee5ee520020a32f5444f357388a72cbed8
ziaul sarker <ziaulsarker@gmail.com>
2023-12-29 01:46:31 -0500

    complete chapter8

--- chapter_8.py
+++ chapter_8.py
@@ -54,7 +54,100 @@ print(album_2)
 
 
 
+# 8.8
+
+artists = {}
+
+while True : 
+  artist_name = input('Artist Name or q to quit ')
+  if artist_name == 'q' : 
+    print('good bye')
+    break
+
+  album_name = input('artist album or q to quit ')
+  if album_name == 'q' : 
+    print('good bye')
+    break
+
+  album = make_album(artist_name, album_name)
+
+  artists[artist_name] = album
+
+  print("album =>", album)
+
+  should_continue = input('want to add more albums (y/n) ')
+
+  if should_continue == 'y' : continue 
+  break
+
+print(artists.get('asap rocky', 'opps not found'))
+
+
+8.9
+def show_msg(my_list: [str]) :
+  for x in my_list :
+    print(x)
+    
+
+show_msg(['artist', 'album'])
+
+
+msg_que = ["yoo", 'whats cracking']
+sent_msg = []
+
+print(msg_que, sent_msg)
+
+def send_msg(msg_que: [str], msg_sent: [str]) :
+  print("inside, fn before loop", msg_que, msg_sent)
+  while msg_que :
+    first_msg = msg_que.pop(0)
+    msg_sent.append(first_msg)
+  
+  print("inside, fn after loop", msg_que, msg_sent)
+  return (msg_que, msg_sent)
 
 
 
+
+print(msg_que, sent_msg, send_msg(msg_que[:], sent_msg[:]))
+
+
+# 8.12
+
+def sandwich(*topings) :
+  for toping in topings :
+    print(f'putting {toping} in the sandwich')
+
+  print(f'now you got a sandwich with the following toppoings {topings}')
+
+
+
+sandwich('beek', 'cheese', 'pickels', 'sauce')
+
+
+# 8.13
+
+def build_profile(fname, lname, **kwargs) :
+  kwargs['first_name'] = fname
+  kwargs['last_name'] = lname
+
+  return kwargs
+
+
+print(build_profile('ziaul', 'sarker', **{'age': 33, "hobby": 'coding', 'lover': 'amena'} ))
+
+# 8.14
+
+def build_car(model, make, **kwargs) :
+  kwargs['model'] = model
+  kwargs['make'] = make
+
+  return kwargs
+
+print(build_car('accord', 2023, color='blue', miles=35_000))
+
+
+
+
+  
 
