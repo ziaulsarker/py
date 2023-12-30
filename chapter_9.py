e6704e536e97a6bd0fd0bea8a0e04992e4cb3103
ziaul sarker <ziaulsarker@gmail.com>
2023-12-30 01:44:39 -0500

    updates

--- chapter_9.py
+++ chapter_9.py
@@ -1,67 +1,67 @@
-# """DOCS MOTHER FUCKERS"""
+"""DOCS MOTHER FUCKERS"""
 
-# # 9.1 
-# class Resturant :
-#   """ Resturant ClassYou Bitchas """
-#   def __init__(self, resturant_name: str, cusine: str) -> None:
-#     self.resturant_name = resturant_name
-#     self.cusine = cusine
-#     self.open_resturant = False
-#     self._number_server = 0
+# 9.1 
+class Resturant :
+  """ Resturant ClassYou Bitchas """
+  def __init__(self, resturant_name: str, cusine: str) -> None:
+    self.resturant_name = resturant_name
+    self.cusine = cusine
+    self.open_resturant = False
+    self._number_server = 0
 
-#   def get_number_server(self): 
-#     print(f"Number Servered {self._number_server}")
+  def get_number_server(self): 
+    print(f"Number Servered {self._number_server}")
 
-#   def set_num_served(self, num_served):
-#     if num_served > self._number_server :
-#       self._number_server = num_served
+  def set_num_served(self, num_served):
+    if num_served > self._number_server :
+      self._number_server = num_served
   
-#   def increment_num_served(self, increment_num_served = 1):
-#     if increment_num_served >= 1 : 
-#       self._number_server += increment_num_served
+  def increment_num_served(self, increment_num_served = 1):
+    if increment_num_served >= 1 : 
+      self._number_server += increment_num_served
 
-#   def make_food(self, food) :
-#     print(food)
+  def make_food(self, food) :
+    print(food)
 
-#   def describe_resturant(self) :
-#     print(f'{self.resturant_name.upper()} serves up {self.cusine}')
+  def describe_resturant(self) :
+    print(f'{self.resturant_name.upper()} serves up {self.cusine}')
 
-#   def open_resturant(self) :
-#     self.open_resturant = True
-#     print(f"Resturant is now open")
+  def open_resturant(self) :
+    self.open_resturant = True
+    print(f"Resturant is now open")
 
-#   def close_resturant(self) :
-#     self.open_resturant = False
-
-
-
-
-# my_store = Resturant("AZ's", "American Fusion")
-# my_store.describe_resturant()
-
-
-# my_store.get_number_server()
-
-# my_store.increment_num_served()
-
-# my_store.get_number_server()
-
-
-# my_store.increment_num_served(10)
-
-# my_store.get_number_server()
-
-# my_store.set_num_served(12)
-
-# my_store.get_number_server()
+  def close_resturant(self) :
+    self.open_resturant = False
 
 
 
-# amena_resturant = Resturant("amenas", "American Fusion")
-# amena_resturant.describe_resturant()
 
-# moms = Resturant("moms", "Benaglie Fusion")
-# moms.describe_resturant()
+my_store = Resturant("AZ's", "American Fusion")
+my_store.describe_resturant()
+
+
+my_store.get_number_server()
+
+my_store.increment_num_served()
+
+my_store.get_number_server()
+
+
+my_store.increment_num_served(10)
+
+my_store.get_number_server()
+
+my_store.set_num_served(12)
+
+my_store.get_number_server()
+
+
+
+amena_resturant = Resturant("amenas", "American Fusion")
+amena_resturant.describe_resturant()
+
+moms = Resturant("moms", "Benaglie Fusion")
+moms.describe_resturant()
 
 
 
@@ -101,6 +101,23 @@ me.reset_login_appemt()
 print(me.login_appemt)
 
 
+
+# 9.6 
+
+class IceCreamStand(Resturant) :
+  def __init__(self, resturant_name: str, cusine: str) :
+    self.flavors = ['vanalia', 'strawberry']
+    Resturant.__init__(self, resturant_name, cusine)
+
+  def available_flavore(self):
+    print(*self.flavors)
+
+
+ice_stand = IceCreamStand('AZ Ice cream', 'desert')
+
+ice_stand.available_flavore()
+
+ice_stand.describe_resturant()
 
 
 
