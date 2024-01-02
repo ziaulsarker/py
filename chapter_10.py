9ea5073b36fc5727e230241f1bdb6a0a3e9a6845
ziaul sarker <ziaulsarker@gmail.com>
2024-01-01 23:05:18 -0500

    chapter 10 some exercises

---
+++ chapter_10.py
@@ -0,0 +1,37 @@
+# 10.1 - 10.3
+from pathlib import Path
+
+file = Path('chapter_10.txt')
+file_text_content = file.read_text()
+
+
+
+print(file_text_content)
+
+for line in file_text_content.splitlines(): 
+  print(line + '\n')
+
+
+line_read = 0
+while line_read < len(file_text_content.splitlines()) :
+  print(file_text_content.splitlines()[line_read], "\n")
+  line_read += 1
+
+file_text_content = file_text_content.replace('python', 'rust')
+
+print(file_text_content)
+
+
+# 10.4 
+
+guest_name = input('Enter guest name : ')
+guests_registry = Path('chapter_10_guests.txt')
+guests_registry.write_text(guest_name + '\n')
+# guests_registry.writelines()
+
+
+
+
+
+
+
