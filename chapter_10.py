a13b447549659ee9a137329ef80f3bedac34ad68
ziaul sarker <ziaulsarker@gmail.com>
2024-01-03 00:02:20 -0500

    complete chapter 10 fuck face

--- chapter_10.py
+++ chapter_10.py
@@ -2,71 +2,71 @@
 import json
 from pathlib import Path
 
-# file = Path('chapter_10.txt')
-# file_text_content = file.read_text()
+file = Path('chapter_10.txt')
+file_text_content = file.read_text()
 
 
 
-# print(file_text_content)
+print(file_text_content)
 
-# for line in file_text_content.splitlines(): 
-#   print(line + '\n')
-
-
-# line_read = 0
-# while line_read < len(file_text_content.splitlines()) :
-#   print(file_text_content.splitlines()[line_read], "\n")
-#   line_read += 1
-
-# file_text_content = file_text_content.replace('python', 'rust')
-
-# print(file_text_content)
+for line in file_text_content.splitlines(): 
+  print(line + '\n')
 
 
-# # 10.4 
+line_read = 0
+while line_read < len(file_text_content.splitlines()) :
+  print(file_text_content.splitlines()[line_read], "\n")
+  line_read += 1
 
-# # one way
-# guest_name = input('Enter guest name : ')
-# guests_registry = open('chapter_10_guests.txt', '+a')
-# guests_registry.writelines(guest_name + '\n')
-# guests_registry.close()
+file_text_content = file_text_content.replace('python', 'rust')
 
-# # another way
-# guest_name = input('Enter guest name : ')
-# guests_registry = Path('chapter_10_guests.txt')
-# guests_registry = guests_registry.open(mode='+a')
-# guests_registry.writelines(guest_name + '\n')
-# guests_registry.close()
-
-# # another way -> much cleaner
-# with Path('chapter_10_guests.txt').open(mode='+a') as f : 
-#   f.writelines(guest_name + '\n')
+print(file_text_content)
 
 
-# 10.6
+# 10.4 
 
-# def add_two_nums() :
-#   while True : 
-#     try :
-#       first_number = int(input('enter first number to add '))
-#     except ValueError :
-#       print('opps that is not a number, please enter a numerical value')
-#       continue 
-#     else : 
-#       break
+# one way
+guest_name = input('Enter guest name : ')
+guests_registry = open('chapter_10_guests.txt', '+a')
+guests_registry.writelines(guest_name + '\n')
+guests_registry.close()
+
+# another way
+guest_name = input('Enter guest name : ')
+guests_registry = Path('chapter_10_guests.txt')
+guests_registry = guests_registry.open(mode='+a')
+guests_registry.writelines(guest_name + '\n')
+guests_registry.close()
+
+# another way -> much cleaner
+with Path('chapter_10_guests.txt').open(mode='+a') as f : 
+  f.writelines(guest_name + '\n')
+
+
+10.6
+
+def add_two_nums() :
+  while True : 
+    try :
+      first_number = int(input('enter first number to add '))
+    except ValueError :
+      print('opps that is not a number, please enter a numerical value')
+      continue 
+    else : 
+      break
   
-#   while True :
-#     try :
-#       second_number = int(input('enter second number to add '))
-#     except ValueError :
-#       print('opps that is not a number, please enter a numerical value')
-#       continue 
-#     else : 
-#       break
+  while True :
+    try :
+      second_number = int(input('enter second number to add '))
+    except ValueError :
+      print('opps that is not a number, please enter a numerical value')
+      continue 
+    else : 
+      break
 
-#   sum_of_nums = first_number + second_number
+  sum_of_nums = first_number + second_number
   
-#   print(f'the sum on {first_number} + {second_number} is : {sum_of_nums}')
+  print(f'the sum on {first_number} + {second_number} is : {sum_of_nums}')
 
 
 file_paths = [*Path('./').glob("*.txt")]
@@ -82,7 +82,7 @@ def readFiles(files: str) -> None :
     else :
       print(file_text + '\n')
 
-# readFiles(file_paths)
+readFiles(file_paths)
 
 json_file_path = 'fav_num.json'
 
