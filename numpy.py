c7e0dec4a4707b5ec2e1e42c68a848b3fe017746
ziaul sarker <ziaulsarker@gmail.com>
2024-01-09 23:43:54 -0500

    revert numpy back

--- numpy.py
+++ numpy.py
@@ -1,4 +1,8 @@
-# 
+import numpy as np
 
-
-print(str.lower('HELLO'))
+# x_train is the input variable (size in 1000 square feet)
+# y_train is the target (price in 1000s of dollars)
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+print(f"x_train = {x_train}")
+print(f"y_train = {y_train}")
