f68587dc087d8024c23ec9c6217c8128ded04a58
ziaul sarker <ziaulsarker@gmail.com>
2024-02-23 22:11:49 -0500

    messing with decorators

--- decorators.py
+++ decorators.py
@@ -10,39 +10,32 @@ def say_loud(fn) :
 
 def say_somthing(fn) : 
   def wrapper(*args, **kwargs) :
-    if (kwargs.get('vol') == 'loud') :
+    # print('kwargs', **kwargs)
+    if (kwargs['vol']== 'loud') :
       print('its loud')
-    elif (kwargs.get('vol') == 'soft') :
+    elif (kwargs['vol'] == 'low') :
       print('its soft')
-    return fn(*args, **kwargs)
-  
+    x = fn(*args, **kwargs)
+    return x 
+  return wrapper
+
+
+
+
+def looger(fn) :
+  def wrapper(*args, **kwargs) :
+    starting = time_ns()
+    print(f'starting {starting}')
+    x = fn(*args, **kwargs)
+    end = time_ns();
+    print(f'finished took {end - starting}')
+    return x
   return wrapper
 
 @say_somthing
+@looger
 def say(name, vol='loud') :
   return f"Hello {name}"
 
-say("hello")
-
-
-
-  
-# def looger(fn) :
-#   def wrapper(*args, **kwargs) :
-#     starting = time_ns()
-#     print(f'starting {starting}')
-#     x = fn(*args, **kwargs)
-#     end = time_ns();
-#     print(f'finished took {end - starting}')
-#     return x
-#   return wrapper
-
-
-# @looger
-# def say(name) :
-#   return f"Hello {name}"
-
-
-
-# print(say(name='world'))
+print(say(name='world', vol='loud'))
 
